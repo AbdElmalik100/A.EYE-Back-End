d99972eefda9957ba53f50024fa138614c1023d6
Abd Elmalik Abd Elghafar <paydaychains100@gmail.com>
2024-02-17 04:25:09 +0200

    filters added on users

--- aeye/settings.py
+++ aeye/settings.py
@@ -67,6 +67,11 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 
